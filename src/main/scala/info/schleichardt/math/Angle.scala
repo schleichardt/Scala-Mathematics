5d9ec71bfdedd27af0bc196826a8204b8f30133e
Michael Schleichardt <michael@schleichardt.net>
2012-04-10 22:18:47 +0200

    refactoring: place implicit conversion for Angle

--- src/main/scala/info/schleichardt/math/Angle.scala
+++ src/main/scala/info/schleichardt/math/Angle.scala
@@ -6,6 +6,8 @@ object Angle {
   def fromDegree(degree: Double) = new Angle(degreeToRadiant(degree))
 
   def degreeToRadiant(degree: Double) = degree * Pi / 180
+
+  implicit def implicitAngleToRadiantDouble(angle: Angle) = angle.radiant
 }
 
 case class Angle(val radiant: Double) {}
--- src/main/scala/info/schleichardt/math/DTMatrix.scala
+++ src/main/scala/info/schleichardt/math/DTMatrix.scala
@@ -1,6 +1,5 @@
 package info.schleichardt.math
 
-import info.schleichardt.math.angleToDouble
 import scala.math._
 
 object DTMatrix {
@@ -15,6 +14,7 @@ object DTMatrix {
 
 class DTMatrix() {
   def toConcreteMatrix(phi: Angle, psi: Angle): ValueMatrix = {
+    import info.schleichardt.math.Angle.implicitAngleToRadiantDouble
     val row1: Seq[Double] = Seq(cos(psi), 0, - sin(psi))
     val row2: Seq[Double] = Seq(- sin(phi) * sin(psi), cos(phi), - sin(phi) * cos(psi))
     val row3: Seq[Double] = Seq(cos(phi) * sin(psi), sin(phi), cos(phi) * cos(psi))
