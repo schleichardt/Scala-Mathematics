0ce897f0d0f47af5aa000647ed51774da8879d76
Michael Schleichardt <Michael@Scheichardt.net>
2012-04-10 08:54:02 +0200

    added x/y coordinate calculation for viewer

--- src/main/scala/info/schleichardt/math/DTMatrix.scala
+++ src/main/scala/info/schleichardt/math/DTMatrix.scala
@@ -3,6 +3,16 @@ package info.schleichardt.math
 import info.schleichardt.math.angleToDouble
 import scala.math._
 
+object DTMatrix {
+  def calculateXCoordinateViewer(distanceToViewer: Double, rotatedVector: MathVector) = {
+    (distanceToViewer * rotatedVector.content(0)) / (distanceToViewer - rotatedVector.content(2))
+  }
+
+  def calculateYCoordinateViewer(distanceToViewer: Double, rotatedVector: MathVector) = {
+    (distanceToViewer * rotatedVector.content(1)) / (distanceToViewer - rotatedVector.content(2))
+  }
+}
+
 class DTMatrix() {
   def toConcreteMatrix(phi: Angle, psi: Angle): ValueMatrix = {
     val row1: Seq[Double] = Seq(cos(phi), 0, - sin(psi))
--- src/main/scala/info/schleichardt/math/VectorsMain.scala
+++ src/main/scala/info/schleichardt/math/VectorsMain.scala
@@ -3,6 +3,9 @@ package info.schleichardt.math
 object VectorsMain {
   def main(args: Array[String]) {
     val dT = new DTMatrix().toConcreteMatrix(Angle.fromDegree(10), Angle.fromDegree(20))
-    println(dT * (new MathVector(Seq(-.5, -.2, -.3))))
+    val rotatedVector: MathVector = dT * (new MathVector(Seq(-.5, -.2, -.3)))
+    println(rotatedVector)
+    println(DTMatrix.calculateXCoordinateViewer(4.0, rotatedVector))
+    println(DTMatrix.calculateYCoordinateViewer(4.0, rotatedVector))
   }
 }
