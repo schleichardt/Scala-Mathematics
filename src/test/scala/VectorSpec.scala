eac0bc0851dc064d9ca49143e3de62c8e7d9e792
Michael Schleichardt <michael@schleichardt.net>
2012-04-10 22:11:33 +0200

    added first test

--- src/test/scala/VectorSpec.scala
+++ src/test/scala/VectorSpec.scala
@@ -6,6 +6,6 @@ import org.junit.runner.RunWith
 @RunWith(classOf[JUnitSuiteRunner])
 class VectorSpec extends Specification with JUnit with ScalaTest {
   "add Vectors" in {
-    MathVector(1,2,3) + MathVector(5, 5, 5) must_== MathVector(6, 7, 9999)
+    MathVector(1,2,3) + MathVector(5, 5, 5) must_== MathVector(6, 7, 8)
   }
 }
