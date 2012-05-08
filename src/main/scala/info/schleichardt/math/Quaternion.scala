0a9a6145d25df0fbb7f1fa92f1a559def936b89e
Michael Schleichardt <michael@schleichardt.net>
2012-05-08 17:26:41 +0200

    Quaternion exercise

--- src/main/scala/info/schleichardt/math/Quaternion.scala
+++ src/main/scala/info/schleichardt/math/Quaternion.scala
@@ -1,12 +1,12 @@
 package info.schleichardt.math
 
-import scala.math.sqrt;
+import scala.math._;
 
 object Quaternion {
    def apply(x0: Double, x1: Double, x2: Double, x3: Double) = new Quaternion(x0, MathVector(x1, x2,x3))
 }
 
-class Quaternion private(val x0: Double, val vector: MathVector) {
+class Quaternion(val x0: Double, val vector: MathVector) {
   def x(other: Quaternion): Quaternion = {
      val resultX0 = (x0 * other.x0) - (vector * other.vector)
      val resultVector = other.vector * x0 + vector * other.x0 + vector.x(other.vector);
@@ -32,5 +32,23 @@ class Quaternion private(val x0: Double, val vector: MathVector) {
   lazy val inverse: Quaternion = {
     import info.schleichardt.math.MathVectorScalar.implicitConversionDoubleToMath
     new Quaternion(x0, vector * (-1.0)) * norm.^(-2)
+  }
+}
+
+object RotationQuaternion {
+  def apply(rotationAngle: Angle, phi: Angle, psi: Angle): Quaternion = {
+    val x0: Double = cos(0.5 * rotationAngle.radiant)
+    val vector = RotationVector(phi, psi) * sin(0.5*rotationAngle)
+    new Quaternion(x0, vector)
+  }
+}
+
+object RotationVector {
+  def apply(phi: Angle, psi: Angle): MathVector = {
+    val x = cos(phi.radiant) * sin(psi.radiant)
+    val y = sin(phi.radiant)
+    val z = cos(phi.radiant) * cos(psi.radiant)
+    println("MathVector(x, y, z) " + MathVector(x, y, z))
+    MathVector(x, y, z)
   }
 }
--- src/main/scala/info/schleichardt/math/exercises/QuaternionenAufgabe.scala
+++ src/main/scala/info/schleichardt/math/exercises/QuaternionenAufgabe.scala
@@ -1,12 +1,28 @@
 package info.schleichardt.math.exercises
 
-import info.schleichardt.math.Quaternion
+import info.schleichardt.math.{Angle, RotationQuaternion, Quaternion}
+import scala.Predef._
 
 
 object QuaternionenAufgabe {
   def main(args: Array[String]) {
     val p1: Quaternion = Quaternion(0.9063, 0.2353, 0.2143, 0.2804).x(Quaternion(0, 2, 3, 4))
     val result = p1.x(Quaternion(0.9063, -0.2352, -0.2113, -0.2804))
-    println(p1)
+
+
+
+    val rotationAngle = Angle.fromDegree(50.0)
+    val phi= Angle.fromDegree(30.0)
+    val psi = Angle.fromDegree(40.0)
+    val rotationQuaternion = RotationQuaternion(rotationAngle, phi, psi)
+    println("rotationQuaternion " + rotationQuaternion)
+    val vector = Quaternion(0, 2, 3, 4)
+    val xRotated = rotationQuaternion.x(vector).x(rotationQuaternion.inverse)
+    println("rotiert: " + xRotated)
+    /*
+    MathVector(x, y, z) (0,5567, 0,5000, 0,6634)T
+rotationQuaternion (0,9063, (0,2353, 0,2113, 0,2804)T)
+rotiert: (0,0000, (2,3404, 2,1798, 4,3326)T)
+     */
   }
 }
